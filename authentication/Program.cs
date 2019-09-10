404231d9fde732c305b1d70f4512a4f9a42ee0e8
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-10 16:33:47 -0400

    authenticatio

---
+++ authentication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the pass code");
+            var code = Console.ReadLine();
+            if(code=="secret")
+            {
+                Console.WriteLine("U r authenticated");
+            }
+            else if(code!="secret")
+            {
+                Console.WriteLine("oops");
+            }
+            
+        }
+    }
+}
